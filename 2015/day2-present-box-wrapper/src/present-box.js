ac111e461feb24634e432cc6ac9194984cd38fe1
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-06 16:40:45 +0530

    refactor: `PresentBox` to have length, width and height instead of dimensions

--- 2015/day2-present-box-wrapper/src/present-box.js
+++ 2015/day2-present-box-wrapper/src/present-box.js
@@ -1,25 +1,42 @@
 class PresentBox {
-  #dimensions;
+  #length;
+  #width;
+  #height;
 
   constructor({ length, width, height }) {
-    this.#dimensions = [length, width, height];
+    this.#length = length;
+    this.#width = width;
+    this.#height = height;
   }
 
   #sumOf(numbers) {
     return numbers.reduce((sum, number) => sum + number, 0);
   }
 
+  #multiply([a, b]) {
+    return a * b;
+  }
+
   #calculateSlack(surfaceAreas) {
     return Math.min(...surfaceAreas);
   }
 
-  areaOfWrapper() {
-    const dimensions = this.#dimensions;
-    const surfaceAreas = dimensions.map((dimension, index) => dimension * dimensions[(index + 1) % 3]);
-    const slack = this.#calculateSlack(surfaceAreas);
-    const totalSurfaceArea = this.#sumOf(surfaceAreas);
+  #getSurfaces() {
+    const l = this.#length;
+    const w = this.#width;
+    const h = this.#height;
 
-    return slack + 2 * totalSurfaceArea;
+    return [[l, w], [w, h], [h, l]];
+  }
+
+  areaOfWrapper() {
+    const surfaces = this.#getSurfaces();
+    const surfaceAreas = surfaces.map(this.#multiply);
+
+    const totalSurfaceArea = 2 * this.#sumOf(surfaceAreas);
+    const slackArea = this.#calculateSlack(surfaceAreas);
+
+    return totalSurfaceArea + slackArea;
   }
 }
 
--- 2015/day2-present-box-wrapper/test/present-box-test.js
+++ 2015/day2-present-box-wrapper/test/present-box-test.js
@@ -5,10 +5,14 @@ const { PresentBox } = require('../src/present-box');
 describe('PresentBox', () => {
   describe('areaOfWrapper', () => {
     it('Should give the total Area correctly for dimensions of any 3 positive dimensions', () => {
-      const dimensions = { length: 2, width: 3, height: 4 };
-      const presentBox = new PresentBox(dimensions);
+      const dimensions1 = { length: 2, width: 3, height: 4 };
+      const presentBox1 = new PresentBox(dimensions1);
 
-      assert.strictEqual(presentBox.areaOfWrapper(), 58);
+      const dimensions2 = { length: 1, width: 1, height: 10 };
+      const presentBox2 = new PresentBox(dimensions2);
+
+      assert.strictEqual(presentBox1.areaOfWrapper(), 58);
+      assert.strictEqual(presentBox2.areaOfWrapper(), 43);
     });
   });
 });
