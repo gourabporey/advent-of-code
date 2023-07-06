ae1caede187322130a3e8eaa3f80c94a9df63256
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-07 01:02:13 +0530

    refactor: improve `findTotalRibbonLength` function for better readability

--- 2015/day2-present-box-wrapper/src/ribbon.js
+++ 2015/day2-present-box-wrapper/src/ribbon.js
@@ -1,17 +1,21 @@
 const { extractDimensions } = require("./dimensions-extractor");
 const { PresentBox } = require("./present-box");
 
+const add = (a, b) => a + b;
+
+const findRibbonLength = (dimension) => {
+  const presentBox = new PresentBox(dimension);
+  const smallestPerimeter = Math.min(...presentBox.getPerimeterOfFaces());
+  const volume = presentBox.volume();
+  return smallestPerimeter + volume;
+};
+
 const findTotalRibbonLength = (rawDimensionsData) => {
   const isNoAreaInformationPresent = rawDimensionsData === undefined || rawDimensionsData.length === 0;
   if (isNoAreaInformationPresent) return 0;
 
   const dimensions = extractDimensions(rawDimensionsData);
-  return dimensions.map((dimension) => {
-    const presentBox = new PresentBox(dimension);
-    const smallestPerimeter = Math.min(...presentBox.getPerimeterOfFaces());
-    const volume = presentBox.volume();
-    return smallestPerimeter + volume;
-  }).reduce((sum, num) => sum + num, 0);
+  return dimensions.map(findRibbonLength).reduce(add, 0);
 }
 
 module.exports = { findTotalRibbonLength };
