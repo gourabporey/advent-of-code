71d591616c9d2ccf5b6946df1b9325324689f132
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-06 17:13:38 +0530

    add: functionality in main.js to use extractDimensions and PresentBox

--- 2015/day2-present-box-wrapper/main.js
+++ 2015/day2-present-box-wrapper/main.js
@@ -1,5 +1,15 @@
+const { extractDimensions } = require("./src/dimensions-extractor");
+const { PresentBox } = require("./src/present-box");
+
 const main = () => {
-  console.log('hello');
+  const dimensions = extractDimensions('4x23x21\n22x29x19');
+  const presentBox1 = new PresentBox(dimensions[0]);
+  const presentBox2 = new PresentBox(dimensions[1]);
+
+  const p1Wrapper = presentBox1.areaOfWrapper();
+  const p2Wrapper = presentBox2.areaOfWrapper();
+
+  console.log('Total Wrapper: ', p1Wrapper + p2Wrapper);
 }
 
 main();
