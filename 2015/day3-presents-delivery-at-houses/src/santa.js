4413aad08298111d953ce442d3736d4c44fb971c
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-08 10:05:27 +0530

    refactor: replace conditionals with lookup for move instructions

--- 2015/day3-presents-delivery-at-houses/src/santa.js
+++ 2015/day3-presents-delivery-at-houses/src/santa.js
@@ -1,23 +1,22 @@
 const { Position } = require('./position');
 const { DeliveryPerson } = require('./delivery-person');
 
-const changeDeliveryPosition = (deliveryPerson, instruction) => {
-  switch (instruction) {
-    case '>': deliveryPerson.moveToEast();
-      break;
-    case '<': deliveryPerson.moveToWest();
-      break;
-    case '^': deliveryPerson.moveToNorth();
-      break;
-    case 'v': deliveryPerson.moveToSouth();
-      break;
-  }
-};
+const createDeliveryPerson = () => new DeliveryPerson(new Position(0, 0));
 
 const getDeliveryPersons = numberOfDeliveryPersons => {
-  return Array.from({ length: numberOfDeliveryPersons }).map(() => {
-    return new DeliveryPerson(new Position(0, 0));
-  });
+  return Array.from({ length: numberOfDeliveryPersons }).map(createDeliveryPerson);
+};
+
+const changeDeliveryPosition = (deliveryPerson, instruction) => {
+  const moveFunctions = {
+    '>': deliveryPerson.moveToEast,
+    '<': deliveryPerson.moveToWest,
+    '^': deliveryPerson.moveToSouth,
+    'v': deliveryPerson.moveToNorth,
+  };
+
+  const moveFunction = moveFunctions[instruction];
+  moveFunction.call(deliveryPerson);
 };
 
 const noOfHousesCovered = ([...instructions], noOfDeliveryPersons = 1) => {
@@ -28,9 +27,9 @@ const noOfHousesCovered = ([...instructions], noOfDeliveryPersons = 1) => {
   housesGifted.add(deliveryPersons[0].getCurrentPosition());
 
   for (const instruction of instructions) {
-    const currentDeliveryPersonPosition = deliveryPersons[0];
-    changeDeliveryPosition(currentDeliveryPersonPosition, instruction);
-    housesGifted.add(currentDeliveryPersonPosition.getCurrentPosition());
+    const currentDeliveryPerson = deliveryPersons[0];
+    changeDeliveryPosition(currentDeliveryPerson, instruction);
+    housesGifted.add(currentDeliveryPerson.getCurrentPosition());
     deliveryPersons.reverse();
   }
 
