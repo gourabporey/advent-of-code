6d90151150183043940dc3d20c46c629bbc0feae
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-08 11:48:51 +0530

    feat: add part2 solution to calculate advent coins

--- 2015/day4-md5/main.js
+++ 2015/day4-md5/main.js
@@ -1,8 +1,10 @@
 const { getSuffixOfMd5Message } = require('./src/md5');
 
 const main = () => {
-  const adventCoins = getSuffixOfMd5Message('iwrupvqb', '00000');
-  console.log('Part 1: Advent Coins:', adventCoins);
+  const adventCoins1 = getSuffixOfMd5Message('iwrupvqb', '00000');
+  const adventCoins2 = getSuffixOfMd5Message('iwrupvqb', '000000');
+  console.log('Part 1: Advent Coins:', adventCoins1);
+  console.log('Part 2: Advent Coins:', adventCoins2);
 };
 
 main();
