b019721d7547cd23c344df918c615f17be339d81
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-09 14:30:12 +0530

    test: `hasVowels` and `hasRepeatingChar`

--- 2015/day5-nice-string/src/nice-string.js
+++ 2015/day5-nice-string/src/nice-string.js
@@ -1,11 +1,23 @@
-const hasVowels = ([...text], count) => {
-  return text.filter((char) => ['a', 'e', 'i', 'o', 'u'].includes(char)).length >= count;
+const hasVowels = ([...text], count = 1) => {
+  return text.filter((char) => ['a', 'e', 'i', 'o', 'u'].includes(char.toLowerCase())).length >= count;
+};
+
+const hasRepeatingChar = ([...text]) => {
+  return text.some((char, charIndex) => char === text[charIndex + 1]);
 };
 
 countNiceStrings = (rawStrings, rules) => {
   if (!rawStrings) return 0;
+
   const strings = rawStrings.split('\n');
-  return strings.filter((string) => hasVowels(string, rules.vowelCount)).length;
+
+  return strings.filter((string) => {
+    const vowelMatchingCriteria = rules.vowelCount ? hasVowels(string, rules.vowelCount) : true;
+
+    const repeatingCharCriteria = rules.repeatingChar ? hasRepeatingChar(string) : true;
+
+    return vowelMatchingCriteria && repeatingCharCriteria;
+  }).length;
 };
 
-module.exports = { countNiceStrings };
+module.exports = { countNiceStrings, hasRepeatingChar, hasVowels };
--- 2015/day5-nice-string/test/nice-string-test.js
+++ 2015/day5-nice-string/test/nice-string-test.js
@@ -1,7 +1,45 @@
 const assert = require('assert');
 const { describe, it } = require('node:test');
 
-const { countNiceStrings } = require('../src/nice-string');
+const { countNiceStrings, hasVowels, hasRepeatingChar } = require('../src/nice-string');
+
+describe('hasVowels', () => {
+  it('should give false for no text', () => {
+    assert.strictEqual(hasVowels(''), false);
+  });
+
+  it('should check the text to have 1 vowel for vowel count', () => {
+    assert.strictEqual(hasVowels('ab'), true);
+    assert.strictEqual(hasVowels('bb'), false);
+  });
+
+  it('should be false when required count of vowel is not present in text', () => {
+    assert.strictEqual(hasVowels('abab', 3), false);
+  });
+
+  it('should be true when required count of vowel is present is present ', () => {
+    assert.strictEqual(hasVowels('gourab', 3), true);
+    assert.strictEqual(hasVowels('I am a boy', 4), true);
+  });
+});
+
+describe('hasRepeatingChar', () => {
+  it('should be false for no text', () => {
+    assert.strictEqual(hasRepeatingChar(''), false);
+  });
+
+  it('should be true for text having only 2 characters repeating', () => {
+    assert.strictEqual(hasRepeatingChar('aa'), true);
+  });
+
+  it('should be false for text having only two different characters', () => {
+    assert.strictEqual(hasRepeatingChar('ab'), false);
+  });
+
+  it('should true a text having repeating chars at any place', () => {
+    assert.strictEqual(hasRepeatingChar('qwertyuiopaaa'), true);
+  });
+});
 
 describe('countNiceStrings', () => {
   it('should give 0 for no strings provided', () => {
@@ -11,5 +49,11 @@ describe('countNiceStrings', () => {
   it('should give 1 for one string with only 3 vowels', () => {
     const rules = { vowelCount: 3 };
     assert.strictEqual(countNiceStrings('aaa', rules), 1);
+    assert.strictEqual(countNiceStrings('ababa', rules), 1);
+  });
+
+  it('should give 1 for 1 string having a repeating character', () => {
+    const rules = { repeatingChar: true };
+    assert.strictEqual(countNiceStrings('bb', rules), 1);
   });
 });
