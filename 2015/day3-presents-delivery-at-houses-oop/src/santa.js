3934db425c6ac30deffae2fdd05991803589c06d
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-11 00:26:33 +0530

    add: methods to Santa class to move to south, east and west

--- 2015/day3-presents-delivery-at-houses-oop/src/santa.js
+++ 2015/day3-presents-delivery-at-houses-oop/src/santa.js
@@ -8,6 +8,18 @@ class Santa {
   moveToNorth() {
     return this.#coordinate.toNorth();
   }
+
+  moveToSouth() {
+    return this.#coordinate.toSouth();
+  }
+
+  moveToWest() {
+    return this.#coordinate.toWest();
+  }
+
+  moveToEast() {
+    return this.#coordinate.toEast();
+  }
 }
 
 module.exports = { Santa };
--- 2015/day3-presents-delivery-at-houses-oop/test/santa-test.js
+++ 2015/day3-presents-delivery-at-houses-oop/test/santa-test.js
@@ -16,4 +16,43 @@ describe('Santa', () => {
       assert.strictEqual(coordinate.toNorth.mock.callCount(), 1);
     });
   });
+
+  describe('moveToSouth', () => {
+    it('should move the santa to the south of its current coordinate', (context) => {
+      const coordinate = {
+        toSouth: context.mock.fn(() => true),
+      };
+
+      const santa = new Santa(coordinate);
+
+      assert.ok(santa.moveToSouth());
+      assert.strictEqual(coordinate.toSouth.mock.callCount(), 1);
+    });
+  });
+
+  describe('moveToEast', () => {
+    it('should move the santa to the east of its current coordinate', (context) => {
+      const coordinate = {
+        toEast: context.mock.fn(() => true),
+      };
+
+      const santa = new Santa(coordinate);
+
+      assert.ok(santa.moveToEast());
+      assert.strictEqual(coordinate.toEast.mock.callCount(), 1);
+    });
+  });
+
+  describe('moveToWest', () => {
+    it('should move the santa to the west of its current coordinate', (context) => {
+      const coordinate = {
+        toWest: context.mock.fn(() => true),
+      };
+
+      const santa = new Santa(coordinate);
+
+      assert.ok(santa.moveToWest());
+      assert.strictEqual(coordinate.toWest.mock.callCount(), 1);
+    });
+  });
 });
