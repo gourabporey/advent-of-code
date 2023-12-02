1c45f55ec9487815c893aed76315b302d88d2842
Gourab Porey <gourabporey@users.noreply.github.com>
2023-12-02 20:52:21 +0530

    Refactor by creating method to get fewest no of cubes in each game

--- 2023/02/src/problem-2.js
+++ 2023/02/src/problem-2.js
@@ -1,21 +1,20 @@
 const { sumOf } = require('../lib/arrays');
 
+const getFewestNoOfCubesOfGame = ({ cubes }) => {
+  return cubes.reduce(
+    (fewestNoOfCubes, { red = 0, green = 0, blue = 0 }) => {
+      return {
+        red: Math.max(fewestNoOfCubes.red, red),
+        green: Math.max(fewestNoOfCubes.green, green),
+        blue: Math.max(fewestNoOfCubes.blue, blue),
+      };
+    },
+    { red: 0, green: 0, blue: 0 }
+  );
+};
+
 const calculateSumOfPowers = (gamesData) => {
-  const fewestRequiredCubesInAllGame = gamesData.map(({ cubes }) => {
-    const fewestCubeCountsPossible = { red: 0, green: 0, blue: 0 };
-
-    cubes.forEach((cube) => {
-      Object.entries(cube).forEach(
-        ([color, count]) =>
-          (fewestCubeCountsPossible[color] = Math.max(
-            fewestCubeCountsPossible[color],
-            count
-          ))
-      );
-    });
-
-    return fewestCubeCountsPossible;
-  });
+  const fewestRequiredCubesInAllGame = gamesData.map(getFewestNoOfCubesOfGame);
 
   const powers = fewestRequiredCubesInAllGame.map(
     ({ red, green, blue }) => red * green * blue
