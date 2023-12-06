3c9306b7306f200df001015aeca30c65f0d2be0b
Gourab Porey <gourabporey@users.noreply.github.com>
2023-12-06 15:09:50 +0530

    Refactor separate function to calculate the no of ways

--- 2023/06/src/problems.js
+++ 2023/06/src/problems.js
@@ -2,14 +2,27 @@ const { multiply } = require('lodash');
 
 const productOf = (numbers) => numbers.reduce(multiply, 1);
 
-const calculateProductOfNumberOfWays = (raceData) => {
-  const noOfWays = raceData.map(([time, distance]) => {
-    for (let i = 0; i <= time; i++) {
-      const distanceTravelled = i * (time - i);
-      if (distanceTravelled > distance) return time + 1 - 2 * i;
-    }
-  });
+const calculateNoOfWaysCrossingRecord = ([time, distance]) => {
+  const totalNoOfTravels = time + 1;
+  let chargingTime = 0;
 
+  while (chargingTime <= time) {
+    // charging time is also the speed of the boat
+    const distanceTravelled = chargingTime * (time - chargingTime);
+
+    if (distanceTravelled > distance) {
+      const countOfFailingTravels = 2 * chargingTime;
+      return totalNoOfTravels - countOfFailingTravels;
+    }
+
+    chargingTime++;
+  }
+
+  return 0;
+};
+
+const calculateProductOfNumberOfWays = (raceData) => {
+  const noOfWays = raceData.map(calculateNoOfWaysCrossingRecord);
   return productOf(noOfWays);
 };
 
